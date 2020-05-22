f92eecd424a776229fb21b61b7d872a24a1c3196
Sébastien Pierre <sebastien.pierre@gmail.com>
2020-05-22 13:06:01 +1200

    [Refactor] Moved stuff in protcol

--- docs/performance.md
+++ docs/performance.md
@@ -7,6 +7,11 @@ testing suite for both HTTP/1 and HTTP/2. Here's a simple test suite:
 # HTTP/1 Testing
 h2load  -n100000 -c100 -m10 --h1 http://localhost:8000/
 
+-  2020/05/22: 5212.12 req/s, 626.07KB/s [Extra]
+-  2020/05/22: 6669.87 req/s, 977.03KB/s [Raw]
+
 # HTTP/2 Testing
 h2load  -n100000 -c100 -m10 http://localhost:8000/
 ```
+
+Todo:
---
+++ src/py/extra/feature/channels.py
@@ -0,0 +1,48 @@
+from typing import TypeVar,Generic,List
+
+T = TypeVar('T')
+
+class Channel(Generic[T]):
+
+	def __init__( self ):
+		self._buffer:List[T] = []
+		self._isOpen = False
+
+	@property
+	def isOpen( self ) -> bool:
+		return self._isOpen
+
+	def open( self ) -> 'Channel':
+		self._isOpen = True
+		return self
+
+	def close( self ) -> 'Channel':
+		self._isOpen = False
+		return self
+
+	def push( self, value:T ) -> T:
+		self._buffer.append(value)
+
+	def consume( self ) -> T:
+		if self._buffer:
+			return self._buffer.pop(0)
+		else:
+			raise ValueError("Channel is empty")
+
+	def peek( self ) -> T:
+		if self._buffer:
+			return self._buffer[0]
+		else:
+			raise ValueError("Channel is empty")
+
+	def has( self ) -> bool:
+		return bool(self._buffer)
+
+	async def join( self ) -> T:
+		"""Waits for a value to come in."""
+		return None
+
+async def consume( channel:Channel ):
+	yield None
+
+# EOF
